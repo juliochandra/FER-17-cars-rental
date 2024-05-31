943bdd16479ca56d716e1e25e1ff4d5259e43dd5
juliochandra <juliochandra@gmail.com>
2024-05-31 14:47:41 +0700

    bug fix, add transfromRespons in apiSlice

--- src/services/apiSlice.js
+++ src/services/apiSlice.js
@@ -12,12 +12,49 @@ export const apiSlice = createApi({
         url: "customer/v2/car",
         params: { ...params },
       }),
+      transformResponse: (response) => {
+        // console.log(response);
+        const transformedCars = response.cars.map((car) => ({
+          id: car.id,
+          category: car.category || "Medium",
+          createdAt: car.createdAt,
+          finish_rent_at: car.finish_rent_at,
+          image:
+            car.image ||
+            "https://images.tokopedia.net/img/KRMmCm/2024/2/7/dd3c8623-bf0e-4283-a8c7-4a6cb9d188c2.jpg",
+          name: car.name || "Mobil",
+          price: car.price || 100000,
+          start_rent_at: car.start_rent_at,
+          status: car.status,
+          updatedAt: car.updatedAt,
+        }));
+        return {
+          ...response,
+          cars: transformedCars,
+        };
+      },
       providesTags: ["Cars"],
     }),
     getCarById: builder.query({
       query: (id) => ({
         url: `customer/car/${id}`,
       }),
+      transformResponse: (response) => {
+        // console.log(response);
+        const transformedCars = {
+          ...response,
+          category: response.category || "Medium",
+          image:
+            response.image ||
+            "https://images.tokopedia.net/img/KRMmCm/2024/2/7/dd3c8623-bf0e-4283-a8c7-4a6cb9d188c2.jpg",
+          start_rent_at: response.start_rent_at,
+          finish_rent_at: response.finish_rent_at,
+          status: response.status,
+          updatedAt: response.updatedAt,
+          createdAt: response.createdAt,
+        };
+        return transformedCars;
+      },
       providesTags: ["Cars"],
     }),
   }),
